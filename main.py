1a6c53e3f149a29e68a6ada5e845ef907666717c
anya-yerramilli <70610759+anya-yerramilli@users.noreply.github.com>
2023-06-16 11:19:02 -0700

    Update main.py

--- main.py
+++ main.py
@@ -36,9 +36,11 @@ st.write('This data comes from the Coffee Quality Institution (CQI) which is a n
 st.write('The dataset was uploaded from a website called Kaggle which has many datasets for people to use. The dataset\'s name is called "Coffee Quality Data (CQI May-2023)". The CQI keeps a database on the web for the purpose of having a resource for coffee researchers, professionals, and others who might need this data. This database contains a variety of information on the production, processing, and sensory evaluation of coffee. It also has other factors that might affect the quality of coffee. Some sensory evaluations (quality scores of the coffee) are Aroma, which refers to the scent or fragrance of the coffee, and Flavor, which is how the coffee tastes (evaluated on sweetness, bitterness, acidity, and other flavor notes).')
 
 # context for dataset (importance, hypothesis)
-st.write('<HYPOTHESIS>')
 st.write('It is important we analyze the various types of coffees found throughout the world to display and inform others of the best coffee options they have. Different people have different preferences, and information regarding certain expiration dates, flavor, or processing methods can help others decide on their desired coffee choices.')
 st.markdown("---")
+
+st.subheader('Hypotheses')
+st.write('We believe that the different factors used in the process of creating coffee affect the coffee's final aroma, flavor, aftertaste, and acidity.')
 
 # cleaning the data
 columns_to_drop = [
